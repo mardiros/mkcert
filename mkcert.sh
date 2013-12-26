b882d853f23203bc403b46bce21f6302c5bdb37f
Guillaume Gauvrit <guillaume@gauvr.it>
2013-12-26 18:37:40 +0100

    Make many certificates

--- mkcert.sh
+++ mkcert.sh
@@ -1,5 +1,10 @@
+!/bin/bash
 echo "*** Create self signed certificate"
 echo "*** from http://www.debian-administration.org/articles/284"
+
+rm index.txt* serial* tmp.pem req.*
+rm -rf private
+rm -rf newcerts
 
 mkdir newcerts private
 echo '8888' > serial 
@@ -7,6 +12,15 @@ touch index.txt
 
 echo "*** Creating a Root Certificate"
 openssl req -new -x509 -extensions v3_ca -keyout private/cakey.pem -out cacert.pem -days 3650 -config ./openssl.01.cnf 
+
+cp private/cacert.pem newcerts/ca-chains.pem
+
+
+read -p "How many certificates ?  " NBCERTS
+
+for (( i=1; i<=$NBCERTS; i++ ))
+do
+read -p "Certificat name ?  " CERTNAME
 
 echo "*** Creating a Certificate Signing Request "
 openssl req -new -nodes -out req.pem -config ./openssl.02.cnf
@@ -18,7 +32,20 @@ mv cert.pem tmp.pem
 
 openssl x509 -in tmp.pem -out cert.pem
 
-cat key.pem cert.pem > key-cert.pem
+cat key.pem cert.pem > newcerts/key-cert-$CERTNAME.pem
+cat cert.pem >> newcerts/ca-chains.pem
 
-rm index.txt* serial*
+mv key.pem newcerts/key-$CERTNAME.pem
+mv cert.pem newcerts/cert-$CERTNAME.pem
+
+done
+
+
+echo "Cleaning"
+#mv private/cakey.pem newcerts/ca-key-private.pem
+#mv cacert.pem newcerts/cacert.pem
+
+#rm index.txt* serial* tmp.pem req.*
+#rm -rf private
+
 echo "done"
